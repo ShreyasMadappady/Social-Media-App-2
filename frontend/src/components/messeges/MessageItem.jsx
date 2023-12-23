69f7f89772e6e49a9a5b346b612027cdf0d33854
ShreyasMS <134408310+ShreyasMadappady@users.noreply.github.com>
2023-12-23 22:31:07 +0530

    Update MessageItem.jsx

--- frontend/src/components/messeges/MessageItem.jsx
+++ frontend/src/components/messeges/MessageItem.jsx
@@ -5,7 +5,7 @@ function MessageItem() {
     <div className="flex items-center gap-3 py-3 px-3  hover:bg-gray-200">
       <img className="w-9 h-9 rounded-full" src={img1} alt="" />
       <div>
-        <h1 className="text-sm font-bold">Jerry Alvarez</h1>
+        <h1 className="text-sm font-bold">Shreyas M S</h1>
         <h2 className="text-xs text-slate-400">Lorem ipsum dolor</h2>
       </div>
     </div>
