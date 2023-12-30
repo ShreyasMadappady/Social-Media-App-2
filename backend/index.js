b4178fb3d1eb2599e9b3f1f01581c909b790da52
Shreyas M S <shreyasmadappady20@gmail.com>
2023-12-30 21:01:02 +0530

    c

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,23 @@ import express from "express";
 
 const app = express();
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.get("/", (req, res) => {
   res.status(200).send("HELLOOO");
+});
+
+app.get("/signin", (req, res) => {
+  const username = req.query.username;
+  const password = req.query.password;
+  res.status(202).json({ username: username, password: password });
 });
 
 app.listen(3000, () => {
--- frontend/src/components/signin/SignIn.jsx
+++ frontend/src/components/signin/SignIn.jsx
@@ -1,9 +1,38 @@
+import { useState } from "react";
+
 function SignIn() {
+  const [username, setUsername] = useState({});
+  const [passsword, setPassword] = useState({});
+
+  const sendRequest = async () => {
+    const res = await fetch(
+      `http://localhost:3000/signin?username=${username}?password=${passsword}`
+    );
+    const data = await res.json();
+    console.log(data);
+  };
+
   return (
     <div>
-      <input type="text" placeholder="Username" /><br />
-      <input type="text" name="" id="" placeholder="Password" /><br />
-      <button>SignIn</button>
+      <input
+        type="text"
+        placeholder="Username"
+        onChange={(e) => {
+          setUsername(e.target.value);
+        }}
+      />
+      <br />
+      <input
+        type="text"
+        name=""
+        id=""
+        placeholder="Password"
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+      />
+      <br />
+      <button onClick={sendRequest}>SignIn</button>
     </div>
   );
 }
